0096520b62858486ad570dd4ef68aa6ef2e2ad6a
İsa Katırcı <ce.isakatirci@gmail.com>
2017-03-01 20:32:56 +0300

    Baslangic

---
+++ Singleton/SingletonVersiyon1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Singleton
+{
+    class SingletonVersiyon1
+    {
+        static void Main()
+        {
+            Singleton singleton1 = Singleton.Instance;
+            Singleton singleton2 = Singleton.Instance;
+            if (singleton1 == singleton2)
+            {
+                Console.WriteLine("Sınıf nesneleri aynı instance'ı gösterir");
+            }
+            Console.ReadKey();
+        }
+    }
+    class Singleton
+    {
+        private static Singleton _instance;
+        protected Singleton()
+        {
+        }
+        public static Singleton Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new Singleton();
+                }
+                return _instance;
+            }
+        }
+    }
+}
